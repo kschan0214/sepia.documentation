ff2ad0a62645246fc80abb2da1b34bda4994bc14
Kwok-shing Chan <k.chan@donders.ru.nl>
2021-11-14 15:31:59 +0100

    update

--- conf.py
+++ conf.py
@@ -26,7 +26,7 @@ author = 'Kwok-Shing Chan'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.7'
+release = '1.0'
 
 
 # -- General configuration ---------------------------------------------------
